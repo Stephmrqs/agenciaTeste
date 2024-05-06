a88ed1b17c96f58965911b04e82351ef21494636
Stephmrqs <142638945+Stephmrqs@users.noreply.github.com>
2024-05-06 20:03:12 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+/* const isProd = process.env.NODE_ENV === "production"
+const prefix = isProd ? "/hp-viagens-final" : ""; */
+
+/* module.exports = {
+    basePath: prefix,
+    assetPrefix: prefix,
+}; */
